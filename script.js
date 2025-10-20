079215f30743277722a2b010f37240d9b8928993
Ankur Gangwar <ankurgangwar4575@gmail.com>
2025-10-20 14:03:30 +0530

    c

---
+++ script.js
@@ -0,0 +1,219 @@
+const B_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const but = document.querySelector("button");
+const to = document.querySelector(".to select");
+const from = document.querySelector(".from select");
+const msg = document.querySelector(".msg");
+const icon = document.querySelector("i");
+icon.addEventListener("click", () => {
+  let tempValue = from.value;
+  from.value = to.value;
+  to.value = tempValue;
+
+  flags(from);
+  flags(to);
+  msg.style.visibility = "hidden";
+});
+but.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".input-amount ").value.trim();
+  const URL = `${B_URL}/${from.value.toLowerCase()}.json`;
+  try {
+    let response = await fetch(URL);
+    let data = await response.json();
+    console.log(data);
+    let rate = data[from.value.toLowerCase()][to.value.toLowerCase()];
+    let totalAmount = amount * rate;
+    msg.style.visibility = "visible";
+    msg.innerText = `${amount} ${from.value} = ${totalAmount} ${to.value}`;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    msg.innerText = "Failed to fetch exchange rate.";
+  }
+});
+
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+const selectOptions = document.querySelectorAll("select");
+for (let select of selectOptions) {
+  for (let key in countryList) {
+    let opt = document.createElement("option");
+    opt.value = key;
+    opt.innerText = key;
+    select.append(opt);
+    if (
+      (select.name == "from" && key == "USD") ||
+      (select.name == "to" && key == "INR")
+    )
+      opt.selected = "selected";
+    select.addEventListener("change", (evt) => {
+      flags(evt.target);
+    });
+  }
+}
+const flags = (el) => {
+  let countryCode = el.value;
+  let flagCode = countryList[countryCode];
+  let newLink = `https://flagsapi.com/${flagCode}/flat/64.png`;
+  let img = el.parentElement.querySelector("img");
+  img.src = newLink;
+};
